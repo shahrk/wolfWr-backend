af42b6947ebf16a8784331692b9ccb206354b1b1
Raj Shah <rshah28@ncsu.edu>
2021-04-04 00:26:53 -0400

    added create update and delete APIs for store

--- src/main/java/com/ncsu/wolfwr/controller/StoreController.java
+++ src/main/java/com/ncsu/wolfwr/controller/StoreController.java
@@ -1,10 +1,15 @@
 package com.ncsu.wolfwr.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ncsu.wolfwr.entity.Store;
@@ -18,8 +23,23 @@ public class StoreController {
 	StoreService storeService;
 	
 	@GetMapping("/{id}")
-	public @ResponseBody Store getStoreById(@PathVariable("id") Integer storeId) {
-		Store store = storeService.getStoreById(storeId);
-		return store;
+	public Store getStoreById(@PathVariable("id") Integer storeId) {
+		return storeService.getStoreById(storeId);
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<Integer> createStore(@RequestBody Store store) {
+		return new ResponseEntity<Integer>(storeService.createStore(store), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public void updateStore(@PathVariable("id") Integer storeId, @RequestBody Store store) {
+		storeService.updateStore(storeId, store);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteStore(@PathVariable("id") Integer storeId) {
+		storeService.deleteStore(storeId);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
--- src/main/java/com/ncsu/wolfwr/service/StoreService.java
+++ src/main/java/com/ncsu/wolfwr/service/StoreService.java
@@ -1,7 +1,5 @@
 package com.ncsu.wolfwr.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -21,13 +19,28 @@ public class StoreService {
 	}
 	
 	public Store getStoreById(Integer storeId) {
-		Optional<Store> storeOptional = storeRepo.findById(storeId);
-		if (storeOptional.isPresent()) {
-			Store store = storeOptional.get();
-			return store;
-		} else {
+		return storeRepo.findById(storeId).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	public Integer createStore(Store store) {
+		if (store.getStoreId() != null) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+		store = this.storeRepo.save(store);
+		return store.getStoreId();
+	}
+	
+	public void updateStore(Integer id, Store store) {
+		if (store.getStoreId() != null && store.getStoreId() != id) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+		if (!this.storeRepo.existsById(id)) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 		}
-//		return storeRepo.findById(storeId).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+		this.storeRepo.save(store);
+	}
+	
+	public void deleteStore(Integer id) {
+		this.storeRepo.deleteById(id);
 	}
 }
