615e2578e992ee38641fed91c6a035327281da7f
Raj Shah <rshah28@ncsu.edu>
2021-04-03 20:23:25 -0400

    initial setup

---
+++ src/main/java/com/ncsu/wolfwr/WolfwrApplication.java
@@ -0,0 +1,13 @@
+package com.ncsu.wolfwr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfwrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolfwrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncsu/wolfwr/WolfwrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncsu.wolfwr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolfwrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
